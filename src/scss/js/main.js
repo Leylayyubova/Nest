c777450482737cafe81c408b0a8385d21f2c68b3
Leyla Ayyubova <102313083+Leylayyubova@users.noreply.github.com>
2024-04-01 03:38:19 +0400

    nest

---
+++ src/scss/js/main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".mySwiper", {
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  });
+  
